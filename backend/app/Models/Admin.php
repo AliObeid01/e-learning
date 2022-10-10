8635722fbd9985596303e688190a0533478b410a
unknown <aliiobeid01@gmail.com>
2022-10-10 04:10:29 +0300

    backend:add Admin model

---
+++ backend/app/Models/Admin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'admins';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        
+    ];
+}
