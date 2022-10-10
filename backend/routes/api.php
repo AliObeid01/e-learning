76cca42b2a4d1bbe715c2a4ec2bdd8e0e7d73571
unknown <aliiobeid01@gmail.com>
2022-10-10 14:18:31 +0300

    backend:update apis routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,11 +10,9 @@ Route::group(["prefix"=> "v1"], function(){
     Route::post('/add_admin', [AdminController::class, 'addAdmin']);
     Route::post('/add_instructor', [AdminController::class, 'addInstructor']);
     Route::post('/add_student', [AdminController::class, 'addStudent']);
-    Route::get('/instructors', [AdminController::class, 'getInstructors']);
     Route::post('/add_course', [AdminController::class, 'addCourse']);
-    Route::get('/instructor_courses', [AdminController::class, 'getInstructorCourses']);
+    Route::get('/instructors', [AdminController::class, 'getInstructors']);
+    Route::get('/instructors_courses', [AdminController::class, 'getInstructorsCourses']);
     Route::get('/students', [AdminController::class, 'getStudents']);
-    Route::get('/courses', [AdminController::class, 'getCourses']);
-    Route::post('/enroll_student', [InstructorController::class, 'enrollStudent']);
-   
+
 });
