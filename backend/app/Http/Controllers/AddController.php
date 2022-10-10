135d5a53c06711d89b55b2f9dceec57941d3366e
unknown <aliiobeid01@gmail.com>
2022-10-10 04:17:57 +0300

    backend:implement the addAdmin function

--- backend/app/Http/Controllers/AddController.php
+++ backend/app/Http/Controllers/AddController.php
@@ -8,5 +8,21 @@ use Validator;
 
 class AddController extends Controller
 {
+
+    public function addAdmin(Request $request){
+
+        $admin= new Admin();
+  
+        $admin->name=$request->name;
+        $admin->email=$request->email;
+        $admin->password= bcrypt($request->password);
+        $admin->save();
+  
+      return response()->json([
+          'data' => $admin
+      ], 201);
+  
+      
+      }
    
 }
