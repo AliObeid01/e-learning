ef5f1a0849494b4be3b575bad1a27398510e0a94
unknown <aliiobeid01@gmail.com>
2022-10-10 09:17:58 +0300

    backend:fix getInstructorCourses function

--- backend/app/Http/Controllers/InstructorController.php
+++ backend/app/Http/Controllers/InstructorController.php
@@ -13,14 +13,26 @@ class InstructorController extends Controller
 
     public function enrollStudent(Request $request) {
         
-        $student = Student::find('63437b9d53d1d266870ce993');
-        $student->enrollments()->attach($request->course_id);
+        $student = Student::find('634566d468fc575d2c09e13b');
+        $student->enrollCourse()->attach($request->course_id);
 
         return response()->json([
             "status" => "Success",
             "message" => "Student has been Enrolled"
         ]);
 
-    }  
+    }
+
+    public function getInstructorCourses(){
+        $data=[];
+        $instructor = Instructor::find('63448be9ea3af1e98e0247d7');//
+        $data[]=$instructor->course()->get()->pluck('name');
+        $students_id=$instructor->course()->get()->pluck('student_id');
+        foreach($students_id as $student){
+          $data[]=$student_name=Student::whereIn('_id',$student)->get()->pluck('name');
+        }
+
+        return $data;
+      }
 
 }
