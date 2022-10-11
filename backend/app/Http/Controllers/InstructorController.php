ffc15fa2aea2cb7bff8d54e18eba0fc81ee6c75d
unknown <aliiobeid01@gmail.com>
2022-10-11 03:41:25 +0300

    backend:add InstructorController

---
+++ backend/app/Http/Controllers/InstructorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use App\Models\Instructor;
+use App\Models\Student;
+use App\Models\Course;
+
+class InstructorController extends Controller
+{
+
+
+
+}
